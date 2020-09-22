133a841bc158d0b767b3a981843614c2d6640646
SaintPeter <rex.schrader@gmail.com>
2020-09-22 15:27:25 -0700

    Replace var with let/const, fix assertion-analyser

--- test-runner.js
+++ test-runner.js
@@ -25,16 +25,16 @@
 *
 */
 
-var analyser = require('./assertion-analyser');
-var EventEmitter = require('events').EventEmitter;
+let analyser = require('./assertion-analyser');
+let EventEmitter = require('events').EventEmitter;
 
-var Mocha = require('mocha'),
-    fs = require('fs'),
-    path = require('path');
-    require("@babel/register");
+const Mocha = require('mocha'),
+      fs = require('fs'),
+      path = require('path');
+require("@babel/register");
 
-var mocha = new Mocha({ timeout: 5000 });
-var testDir = './tests'
+let mocha = new Mocha({ timeout: 5000 });
+let testDir = './tests'
 
 
 // Add each .js file to the mocha instance
@@ -48,21 +48,21 @@ fs.readdirSync(testDir).filter(function(file){
     );
 });
 
-var emitter = new EventEmitter();  
+let emitter = new EventEmitter();
 emitter.run = function() {
 
-  var tests = [];
-  var context = "";
-  var separator = ' -> ';
+  let tests = [];
+  let context = "";
+  let separator = ' -> ';
   // Run the tests.
   try {
-  var runner = mocha.ui('tdd').run()
+  let runner = mocha.ui('tdd').run()
     .on('test end', function(test) {
         // remove comments
-        var body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
+        let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
         // collapse spaces
         body = body.replace(/\s+/g,' ');
-        var obj = {
+        let obj = {
           title: test.title,
           context: context.slice(0, -separator.length),
           state: test.state,
